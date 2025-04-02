5c1ad01af7b3cc87745080b5ccc7eb8b7203e15f
Juandicode <juandiegomhs@mail.com>
2025-04-02 11:46:31 -0300

    amenities fixed

--- part3/app/api/v1/amenities.py
+++ part3/app/api/v1/amenities.py
@@ -1,6 +1,7 @@
 from flask_restx import Namespace, Resource, fields
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask import request
+from app.services import facade
 
 api = Namespace('amenities', description='Amenity operations')
 
