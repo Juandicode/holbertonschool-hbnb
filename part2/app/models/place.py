2ca89d5eac59f6d89be365ad31247d4b9a9e46d8
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-06-13 10:13:30 -0300

    my classes

--- part2/app/models/place.py
+++ part2/app/models/place.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 
+class Place(BaseModel):
+    """Place model"""
+    def __init__(self, title: str, price: float, latitude: float, longitude: float, owner: User, description: str = ""):
+        super().__init__()  # hereda id, created_at, updated_at
+        self.title = self.validate_title(title)
+        self.description = description
+        self.price = self.validate_price(price)
+        self.latitude = self.validate_latitude(latitude)
+        self.longitude = self.validate_longitude(longitude)
+        self.owner = self.validate_owner(owner)
