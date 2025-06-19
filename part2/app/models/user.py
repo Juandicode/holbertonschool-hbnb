4938ec40756faf46c911eca61268ebf8d8abae1a
Juandicode <juandiegomhs@mail.com>
2025-06-19 12:00:20 -0300

    probando get y post funcionan correctamente en user

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -2,7 +2,7 @@
 """User class"""
 
 import uuid
-from part2.app.models.base import BaseModel
+from app.models.base import BaseModel
 
 class User(BaseModel):
     def __init__(self, first_name: str, last_name: str, email: str, is_admin: bool = False):
