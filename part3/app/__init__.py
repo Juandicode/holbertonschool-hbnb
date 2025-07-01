4ed575feacaa7fb17d99bd8e8a5984439c842710
juandicode <juandiegomhs@gmail.com>
2025-07-01 18:00:34 -0300

    authpy

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask
-from flask_bcrypt import Bcrypt # new import from bcrypt
-from app.api import api  # <-- importás el Api ya configurado con los namespaces
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
-bcrypt = Bcrypt() # create bcrypt instance
+from app.api import api
+from config import DevelopmentConfig  # ✅ importás la clase, no el string
 
-def create_app(config_class="config.DevelopmentConfig"):
+bcrypt = Bcrypt()
+jwt = JWTManager()
+
+def create_app(config_class=DevelopmentConfig):  # ✅ usás la clase
     app = Flask(__name__)
-    app.config.from_object(config_class)
-
-    api.init_app(app)  # <-- lo registrás en la app
+    app.config.from_object(config_class)  # ✅ correcto, ahora sí es una clase
+    
     bcrypt.init_app(app)
-
+    jwt.init_app(app)
+    
+    api.init_app(app)
+    
     return app
